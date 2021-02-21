f2612edab0db05e25ec1588bfc5cadf837a49d78
Fabio <4898010+gatto@users.noreply.github.com>
2021-02-21 12:05:21 +0100

    Create classification.py

---
+++ classification.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import utils
+
+# tracks = utils.load("data/tracks.csv")
+# genres = utils.load("data/genres.csv")
+# features = utils.load("data/features.csv")
+artists = utils.load("data/raw_artists.csv")
+genres = utils.load("data/genres.csv")
