61d0445c91dffd0c4631df68fa9619c26f349eb2
Fabio <4898010+gatto@users.noreply.github.com>
2021-02-21 20:03:59 +0100

    works on windows

--- classification.py
+++ classification.py
@@ -1,13 +1,13 @@
+from pathlib import Path
+
 import pandas as pd
+
 import utils
 
-# tracks = utils.load("data/tracks.csv")
-# genres = utils.load("data/genres.csv")
-# features = utils.load("data/features.csv")
-artists = utils.load("data/raw_artists.csv")
-genres = utils.load("data/genres.csv")
-echonest = utils.load("data/echonest.csv")
-tracks = utils.load("data/tracks.csv")
+genres = utils.load(Path("data/genres.csv"))
+echonest = utils.load(Path("data/echonest.csv"))
+tracks = utils.load(Path("data/tracks.csv"))
+artists = utils.load(Path("data/raw_artists.csv"))
 
-print(echonest.info())
-print(echonest.head())
+print(tracks.info())
+print(tracks.head())
--- utils.py
+++ utils.py
@@ -1,28 +1,61 @@
+import ast
+
 import pandas as pd
-import os.path
 
 
 def load(filepath):
 
-    filename = os.path.basename(filepath)
+    filename = filepath.name
 
     if "features" in filename:
-        print(filename)
-        print("1")
-        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+        print("using features")
+        df = pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
 
-    if "echonest" in filename:
-        print(filename)
-        print("2")
-        return pd.read_csv(filepath, index_col=0, header=[0, 1])
+    elif "echonest" in filename:
+        print("using echonest")
+        df = pd.read_csv(filepath, index_col=0, header=[0, 1])
 
-    if ("genres" or "albums" or "artists") in filename:
-        print(filename)
-        print("3")
-        return pd.read_csv(filepath, index_col=0)
+    elif any(x in filename for x in ("genres", "albums", "artists")):
+        print("using anyof")
+        df = pd.read_csv(filepath, index_col=0)
 
-    if "tracks" in filename:
-        print(filename)
-        print("4")
-        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1], low_memory=False)
-        return tracks
+    elif "tracks" in filename:
+        print("using tracks")
+        df = pd.read_csv(filepath, index_col=0, header=[0, 1], low_memory=False)
+
+        columns = [
+            ("track", "tags"),
+            ("album", "tags"),
+            ("artist", "tags"),
+            ("track", "genres"),
+            ("track", "genres_all"),
+        ]
+        for column in columns:
+            df[column] = df[column].map(ast.literal_eval)
+
+        columns = [
+            ("track", "date_created"),
+            ("track", "date_recorded"),
+            ("album", "date_created"),
+            ("album", "date_released"),
+            ("artist", "date_created"),
+            ("artist", "active_year_begin"),
+            ("artist", "active_year_end"),
+        ]
+        for column in columns:
+            df[column] = pd.to_datetime(df[column])
+
+        columns = [
+            ("track", "genre_top"),
+            ("track", "license"),
+            ("album", "type"),
+            ("album", "information"),
+            ("artist", "bio"),
+        ]
+        for column in columns:
+            df[column] = df[column].astype("category")
+    else:
+        print(f"Something bad just happened with {filename}.")
+
+    df = df.convert_dtypes()
+    return df
