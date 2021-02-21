d7a479c1ae778fffd61b69318782ebccf9d435a8
Fabio <4898010+gatto@users.noreply.github.com>
2021-02-21 16:42:48 +0100

    added tracks

--- classification.py
+++ classification.py
@@ -7,6 +7,7 @@ import utils
 artists = utils.load("data/raw_artists.csv")
 genres = utils.load("data/genres.csv")
 echonest = utils.load("data/echonest.csv")
+tracks = utils.load("data/tracks.csv")
 
 print(echonest.info())
 print(echonest.head())
--- utils.py
+++ utils.py
@@ -7,10 +7,22 @@ def load(filepath):
     filename = os.path.basename(filepath)
 
     if "features" in filename:
+        print(filename)
+        print("1")
         return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
 
     if "echonest" in filename:
+        print(filename)
+        print("2")
         return pd.read_csv(filepath, index_col=0, header=[0, 1])
 
-    if "genres" or "albums" or "artists" in filename:
+    if ("genres" or "albums" or "artists") in filename:
+        print(filename)
+        print("3")
         return pd.read_csv(filepath, index_col=0)
+
+    if "tracks" in filename:
+        print(filename)
+        print("4")
+        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1], low_memory=False)
+        return tracks
