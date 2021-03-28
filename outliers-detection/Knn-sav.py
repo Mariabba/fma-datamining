fa7df7bbed12c700b589f69b8e47c2f3b337028c
Saverio Telera <s.telera@studenti.uniba.it>
2021-03-28 17:45:31 +0200

    knn finished

--- outliers-detection/Knn-sav.py
+++ outliers-detection/Knn-sav.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 # sklearn
+from pyod.utils.example import visualize
 from sklearn import metrics
 from sklearn.preprocessing import (
     MinMaxScaler,
@@ -52,78 +53,13 @@ from pathlib import Path
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import seaborn as sns
 from pyod.models.knn import KNN
-
-
-# FUNCTION
-
-
-def draw_confusion_matrix(Clf, X, y):
-    titles_options = [
-        ("Confusion matrix, without normalization", None),
-        ("Normalized confusion matrix", "true"),
-    ]
-
-    for title, normalize in titles_options:
-        disp = plot_confusion_matrix(Clf, X, y, cmap="OrRd", normalize=normalize)
-        disp.ax_.set_title(title)
-
-    plt.show()
-
-
-def conf_mat_disp(confusion_matrix, disp_labels):
-    disp = ConfusionMatrixDisplay(
-        confusion_matrix=confusion_matrix, display_labels=disp_labels
-    )
-
-    disp.plot(cmap="OrRd")
-
-
-def draw_roc_curve(Y_test, Y_pred, diz, k):
-    fig, ax = plt.subplots()  # figsize = (13,30)
-
-    fpr, tpr, _ = roc_curve(Y_test, Y_pred)
-    roc_auc = auc(fpr, tpr)
-    roc_auc = roc_auc_score(Y_test, Y_pred, average=None)
-
-    diz[k] = {"fpr": fpr.tolist(), "tpr": tpr.tolist(), "roc": roc_auc}
-
-    ax.plot(fpr, tpr, color="#994D00", label="ROC curve (area = %0.2f)" % (roc_auc))
-    ax.plot([0, 1], [0, 1], "r--")
-    ax.set_xlim([0.0, 1.0])
-    ax.set_ylim([0.0, 1.05])
-    ax.set_xlabel("False Positive Rate")
-    ax.set_ylabel("True Positive Rate")
-    ax.set_title("Roc curve of the model")
-    ax.tick_params(axis="both")
-    ax.legend(loc="lower right", title="AUC", fontsize=14, frameon=True)
-
-    fig.tight_layout()
-    plt.show()
-
-
-def draw_precision_recall_curve(Y_test, Y_pred):
-    fig, ax = plt.subplots()
-
-    pr_ap = average_precision_score(Y_test, Y_pred, average=None)
-    precision, recall, ap_thresholds = precision_recall_curve(Y_test, Y_pred)
-
-    ax.plot(precision, recall, color="#994D00", label="AP %0.4f" % (pr_ap))
-    # ax.plot([0, 1], [no_skill, no_skill], 'r--', label='%0.4f' % no_skill)
-    ax.set_xlim([0.0, 1.0])
-    ax.set_ylim([0.0, 1.05])
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.tick_params(axis="both")
-    ax.legend(loc="upper right", title="AP", frameon=True)
-    ax.set_title("Model Precision-Recall curve")
-
-    fig.tight_layout()
-    plt.show()
-
+from sklearn.decomposition import PCA
+from sklearn.neighbors import NearestNeighbors
 
 # DATASET
 df = utils.load("../data/tracks.csv", dummies=True, buckets="continuous", fill=True)
 column2drop = [
+    ("album", "type"),
     ("album", "tags"),
     ("artist", "tags"),
     ("track", "tags"),
@@ -165,66 +101,69 @@ def normalize(feature):
 for col in df.columns:
     normalize(col)
 
+"""
 # FACCIO  IL PLOTTING BOXPLOT del Df completo
 plt.figure(figsize=(20, 25))
 b = sns.boxplot(data=df, orient="h")
 b.set(ylabel="Class", xlabel="Normalization Value")
 plt.show()
+"""
 
-class_name = ("album", "type")
-attributes = [col for col in df.columns if col != class_name]
-X = df[attributes].values
-y = df[class_name]
+X = df.values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100, stratify=y)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100, stratify=y)
 
+# instantiate model
+nbrs = NearestNeighbors(n_neighbors = 3)
+# fit model
+nbrs.fit(X)
+
+# distances and indexes of k-neaighbors from model outputs
+distances, indexes = nbrs.kneighbors(X)
+# plot mean of k-distances of each observation
+plt.plot(distances.mean(axis =1))
+plt.show()
+
+# visually determine cutoff values > 10
+outlier_index = np.where(distances.mean(axis = 1) > 0.8)
+print(outlier_index)
+
+# filter outlier values
+outlier_values = df.iloc[outlier_index]
+print(outlier_values.describe())
+print(outlier_values.value_counts())
+print(outlier_values.info())
+
+
+"""
 clf = KNN()
 clf.fit(X)
 
+
+print(clf.decision_scores_)
+
+
+anomlay_score = clf.decision_function(X)
+
+outliers = clf.predict(X)
+np.unique(outliers, return_counts=True)
+
+np.mean(anomlay_score[np.where(outliers==1)])
+
+np.mean(anomlay_score[np.where(outliers==0)])
+
+np.mean(clf.decision_scores_[np.where(outliers==1)])
+
+np.mean(clf.decision_scores_[np.where(outliers==0)])
+
+plt.hist(clf.decision_scores_, bins=20)
+plt.axvline(np.min(clf.decision_scores_[np.where(outliers==1)]), c='k')
+plt.show()
+
+#funzione per plottare il df in 2 dimensioni
+pca = PCA(n_components=2)
+pca.fit(X)
+X_train_pca = pca.transform(X)
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], cmap=plt.cm.prism, edgecolor='k', alpha=0.7)
+plt.show()
 """
-# Create KNN Object.
-knn = KNeighborsClassifier(
-    n_neighbors=2, p=1
-)  # dopo la grid search il mio setting è n = 2 e p =1
-# Create x and y variables.
-x = df.drop(columns=[("album", "type")])
-y = df[("album", "type")]
-# Split data into training and testing.
-X_train, X_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=0.25)
-print(X_train.shape, X_test.shape)
-# Training the model.
-knn.fit(X_train, y_train)
-# Predict test data set.
-Y_pred = knn.predict(X_test)
-
-
-# Checking performance our model with classification report.
-
-print("Accuracy:", metrics.accuracy_score(y_test, Y_pred))
-# confusion matrix
-print("\033[1m" "Confusion matrix" "\033[0m")
-
-
-draw_confusion_matrix(knn, X_test, y_test)
-
-print()
-
-
-print("\033[1m" "Classification report test" "\033[0m")
-print(classification_report(y_test, Y_pred))
-
-print()
-
-print("\033[1m" "Metrics" "\033[0m")
-
-
-print("Accuracy %s" % accuracy_score(y_test, Y_pred))
-
-print("F1-score %s" % f1_score(y_test, Y_pred, average="weighted", zero_division=0))
-
-print(
-    "Precision %s"
-    % precision_score(y_test, Y_pred, average="weighted", zero_division=0)
-)
-"""
-#print("Recall %s" % recall_score(y_test, Y_pred, average="weighted", zero_division=0))
