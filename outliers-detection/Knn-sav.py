03961ddfac41dba4c30a921032e414d12d9ab64b
Saverio Telera <s.telera@studenti.uniba.it>
2021-03-30 13:28:15 +0200

    knn update

--- outliers-detection/Knn-sav.py
+++ outliers-detection/Knn-sav.py
@@ -108,11 +108,13 @@ b = sns.boxplot(data=df, orient="h")
 b.set(ylabel="Class", xlabel="Normalization Value")
 plt.show()
 """
+#X = df.drop(columns=[("album", "type")])
+#y = df[("album", "type")]
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100, stratify=y)
 
 X = df.values
 
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100, stratify=y)
-
+"""
 # instantiate model
 nbrs = NearestNeighbors(n_neighbors = 3)
 # fit model
@@ -133,37 +135,36 @@ outlier_values = df.iloc[outlier_index]
 print(outlier_values.describe())
 print(outlier_values.value_counts())
 print(outlier_values.info())
-
-
 """
+
+# train kNN detector
+clf_name = 'KNN'
 clf = KNN()
 clf.fit(X)
+# If you want to see the predictions of the training data, you can use this way:
+y_train_scores = clf.decision_scores_
+print("Prediction on the training data: ")
+print(y_train_scores)
 
+# Now we have the trained K-NN model, let's apply to the test data to get the predictions
+y_test_pred = clf.predict(X) # outlier labels (0 or 1)
+# Because it is '0' and '1', we can run a count statistic.
+unique, counts = np.unique(y_test_pred, return_counts=True)
+print("Prediction on the test data: ")
+print(dict(zip(unique, counts)))
 
-print(clf.decision_scores_)
+# And you can generate the anomaly score using clf.decision_function:
+y_test_scores = clf.decision_function(X)
+print("Anomaly score: ")
+plt.hist(y_test_scores, bins='auto')  # arguments are passed to np.histogram
+plt.title("Histogram with 'auto' bins")
+plt.show()
 
-
-anomlay_score = clf.decision_function(X)
-
-outliers = clf.predict(X)
-np.unique(outliers, return_counts=True)
-
-np.mean(anomlay_score[np.where(outliers==1)])
-
-np.mean(anomlay_score[np.where(outliers==0)])
-
-np.mean(clf.decision_scores_[np.where(outliers==1)])
-
-np.mean(clf.decision_scores_[np.where(outliers==0)])
+print(np.mean(y_test_scores[np.where(y_test_pred==1)]))
+print(np.mean(y_test_scores[np.where(y_test_pred==0)]))
+print(np.mean(clf.decision_scores_[np.where(y_test_pred==1)]))
+print(np.mean(clf.decision_scores_[np.where(y_test_pred==0)]))
 
 plt.hist(clf.decision_scores_, bins=20)
-plt.axvline(np.min(clf.decision_scores_[np.where(outliers==1)]), c='k')
+plt.axvline(np.min(clf.decision_scores_[np.where(y_test_pred==1)]), c='k')
 plt.show()
-
-#funzione per plottare il df in 2 dimensioni
-pca = PCA(n_components=2)
-pca.fit(X)
-X_train_pca = pca.transform(X)
-plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], cmap=plt.cm.prism, edgecolor='k', alpha=0.7)
-plt.show()
-"""
