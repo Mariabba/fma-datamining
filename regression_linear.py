c1d272984dd84412ef87e07dfa11814a232fed7f
Fabio <4898010+gatto@users.noreply.github.com>
2021-04-05 17:11:54 +0200

    better graphing with seaborn

--- regression_linear.py
+++ regression_linear.py
@@ -4,6 +4,7 @@ import pandas as pd
 from rich import pretty, print
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import seaborn as sns
 
 import utils
 
@@ -29,12 +30,33 @@ print("R2: %.3f" % r2_score(test_y, y_pred))
 print("MSE: %.3f" % mean_squared_error(test_y, y_pred))
 print("MAE: %.3f" % mean_absolute_error(test_y, y_pred))
 
-# i don't know what the fuck the graph code does - copypaste from Guidotti
+# i don't know what the fuck this graph code does - copypaste from Guidotti
+"""
 x_values = sorted(test_x[("track", "listens")])
 y_values = y_pred[np.argsort(test_x[("track", "listens")])]
 plt.scatter(test_x, test_y, color="black")
 plt.plot(x_values, y_values, color="blue", linewidth=3)
 plt.title("Scatterplot with regression line")
+plt.show()
+"""
+
+# this is better graphing, using seaborn
+train_x = train_x.join(train_y).astype(float)
+train_x.columns = train_x.columns.get_level_values(1)
+sns.set_theme(style="darkgrid")
+
+# Show the results of a linear regression within each dataset
+g = sns.jointplot(
+    x="listens",
+    y="favorites",
+    data=train_x,
+    kind="reg",
+    truncate=False,
+    color="m",
+    ci=None,
+    height=7,
+    scatter_kws={"s": 20},
+)
 plt.show()
 
 exit()
