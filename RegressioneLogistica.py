bfb70d19c0b72417c724b7f3dd99e6954cbb1992
Saverio Telera <s.telera@studenti.uniba.it>
2021-04-09 19:32:30 +0200

    Logistic Regression

---
+++ RegressioneLogistica.py
@@ -0,0 +1,124 @@
+import utils
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    classification_report,
+    confusion_matrix,
+)
+class_name=('album', 'type')
+
+df = utils.load_tracks(
+    "data/tracks.csv", outliers=False, buckets="continuous"
+)
+
+column2drop = [
+    ("track", "language_code"),
+    ("track", "license"),
+]
+
+df.drop(column2drop, axis=1, inplace=True)
+
+def normalize(feature):
+    scaler = StandardScaler()
+    df[feature] = scaler.fit_transform(df[[feature]])
+
+columns_to_normalize = [col for col in df.columns if col != class_name]
+
+for col in columns_to_normalize:
+    normalize(col)
+
+# feature to reshape
+label_encoders = dict()
+column2encode = [
+    ("album", "type"),
+]
+
+for col in column2encode:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    label_encoders[col] = le
+
+
+print(df.info())
+
+
+attributes = [col for col in df.columns if col != class_name]
+X = df[attributes].values
+y = df[class_name]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='lbfgs', max_iter=1000)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print('Accuracy %s' % accuracy_score(y_test, y_pred))
+print('F1-score %s' % f1_score(y_test, y_pred, average=None))
+print(classification_report(y_test, y_pred))
+
+
+"""
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot
+
+
+# get the dataset
+def get_dataset():
+    attributes = [col for col in df.columns if col != class_name]
+    X = df[attributes].values
+    y = df[class_name]
+    return X, y
+
+
+# get a list of models to evaluate
+def get_models():
+    models = dict()
+    for p in [0.0, 0.0001, 0.001, 0.01, 0.1, 1.0]:
+        # create name for model
+        key = '%.4f' % p
+        # turn off penalty in some cases
+        if p == 0.0:
+            # no penalty in this case
+            models[key] = LogisticRegression(multi_class='multinomial', solver='lbfgs', penalty='none', max_iter=1000)
+        else:
+            models[key] = LogisticRegression(multi_class='multinomial', solver='lbfgs', penalty='l2', C=p, max_iter=1000)
+    return models
+
+
+# evaluate a give model using cross-validation
+def evaluate_model(model, X, y):
+    # define the evaluation procedure
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    # evaluate the model
+    scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    return scores
+
+
+# define dataset
+X, y = get_dataset()
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+    # evaluate the model and collect the scores
+    scores = evaluate_model(model, X, y)
+    # store the results
+    results.append(scores)
+    names.append(name)
+    # summarize progress along the way
+    print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+# plot model performance for comparison
+pyplot.boxplot(results, labels=names, showmeans=True)
+pyplot.show()
+"""
