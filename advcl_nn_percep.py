3f4d7dd027cadef52772d6354305379791edeeba
Marianna Abbattista <37538269+Mariabba@users.noreply.github.com>
2021-04-10 16:44:23 +0200

    NN start Perceptron

--- advcl_nn_percep.py
+++ advcl_nn_percep.py
@@ -5,24 +5,100 @@ import pandas as pd
 
 import utils
 
-dfs = utils.load_tracks_xyz(extractclass=("track", "listens"))
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.inspection import permutation_importance
+
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    classification_report,
+    roc_curve,
+    auc,
+    roc_auc_score,
+    plot_confusion_matrix,
+)
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, StandardScaler
+from sklearn.neural_network import MLPClassifier
+
+import utils
 
 
-scaler = StandardScaler()
-scaler.fit(dfs["train_x"])
-train_x = scaler.transform(dfs["train_x"])
-test_x = scaler.transform(dfs["test_x"])
-dfs["train_y"] = pd.DataFrame(dfs["train_y"])
-train_y = scaler.transform(dfs["train_y"])
+def draw_confusion_matrix(Clf, X, y):
+    titles_options = [
+        ("Confusion matrix, without normalization", None),
+        ("Random Forest Confusion matrix", "true"),
+    ]
 
+    for title, normalize in titles_options:
+        disp = plot_confusion_matrix(Clf, X, y, cmap="Oranges", normalize=normalize)
+        disp.ax_.set_title(title)
+
+    plt.show()
+
+
+# DATASET
+df = utils.load_tracks(
+    "data/tracks.csv", dummies=True, buckets="discrete", fill=True, outliers=True
+)
+
+print(df["album", "type"].unique())
+
+# feature to reshape
+label_encoders = dict()
+column2encode = [
+    ("track", "language_code"),
+    ("album", "listens"),
+    ("album", "type"),
+    ("track", "license"),
+    ("album", "comments"),
+    ("album", "date_created"),
+    ("album", "favorites"),
+    ("artist", "comments"),
+    ("artist", "date_created"),
+    ("artist", "favorites"),
+    ("track", "comments"),
+    ("track", "date_created"),
+    ("track", "duration"),
+    ("track", "favorites"),
+    ("track", "interest"),
+    ("track", "listens"),
+]
+for col in column2encode:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    label_encoders[col] = le
+df.info()
+
+class_name = ("album", "type")
+
+attributes = [col for col in df.columns if col != class_name]
+X = df[attributes].values
+y = df[class_name]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=100, stratify=y
+)
+
+"""NN single layer base PERCEPTRON"""
 clf = MLPClassifier(random_state=0)
+clf.fit(X_train, y_train)
 
-clf.fit(train_x, train_y)
+# Apply on the training set
+print("Apply  on the training set: \n")
+Y_pred = clf.predict(X_train)
+print("Accuracy  %s" % accuracy_score(y_train, Y_pred))
+print("F1-score %s" % f1_score(y_train, Y_pred, average=None))
+print(classification_report(y_train, Y_pred))
 
-exit()
-
-y_pred = clf.predict(test_x)
-
+# Apply on the test set and evaluate the performance
+print("Apply on the test set and evaluate the performance: \n")
+y_pred = clf.predict(X_test)
 print("Accuracy %s" % accuracy_score(y_test, y_pred))
-print("F1-score %s" % f1_score(y_test, y_pred, average=None))
+print("F1-score  %s" % f1_score(y_test, y_pred, average=None))
 print(classification_report(y_test, y_pred))
+
+draw_confusion_matrix(clf, X_test, y_test)
