d845ed5653b32e8be4526eeb8d8443333c6163ed
Fabio <4898010+gatto@users.noreply.github.com>
2021-04-11 13:03:48 +0200

    finita regression
    potrebbe essere migliorata

--- regression_linear.py
+++ regression_linear.py
@@ -1,45 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from rich import pretty, print
+from rich.console import Console
+from rich.table import Table
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
+
 import utils
 
 pretty.install()
+console = Console()
+# outputs in table format
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("Method", style="green")
+# table.add_column("Coefficients")
+table.add_column("R^2", justify="right")
+table.add_column("MSE", justify="right")
+table.add_column("MAE", justify="right")
 
 # Univariate
-train_x, train_y, vali_x, vali_y, test_x, test_y = utils.load_tracks_xyz(
-    buckets="continuous", extractclass=("track", "favorites")
+train_x, train_y, test_x, test_y = utils.load_tracks_xyz(
+    buckets="continuous", extractclass=("track", "favorites"), splits=2
 ).values()
 
 train_x = train_x[[("track", "listens")]]
 test_x = test_x[[("track", "listens")]]
-vali_x = vali_x[[("track", "listens")]]
 
 reg = LinearRegression()
 reg.fit(train_x, train_y)
 
-print("Coefficients: \n", reg.coef_)
-print("Intercept: \n", reg.intercept_)
-
+print("[magenta]Univariate[/magenta]")
+print("Intercept:\t", reg.intercept_)
+print("Coefficients:\t", reg.coef_[0])
 y_pred = reg.predict(test_x)
-print("R2: %.3f" % r2_score(test_y.astype(float), y_pred))
-print("MSE: %.3f" % mean_squared_error(test_y.astype(float), y_pred))
-print("MAE: %.3f" % mean_absolute_error(test_y.astype(float), y_pred))
+
+table.add_row(
+    "Univariate linear",
+    f"{r2_score(test_y.astype(float), y_pred):.3f}",
+    f"{mean_squared_error(test_y.astype(float), y_pred):.3f}",
+    f"{mean_absolute_error(test_y.astype(float), y_pred):.3f}",
+)
 
 # Graphing
-train_x = train_x.join(train_y).astype(float)
-train_x.columns = train_x.columns.get_level_values(1)
+train_graphing = train_x.join(train_y).astype(float)
+train_graphing.columns = train_graphing.columns.get_level_values(1)
 sns.set_theme(style="darkgrid")
-
-# Show the results of a linear regression within each dataset
 g = sns.jointplot(
     x="listens",
     y="favorites",
-    data=train_x,
+    data=train_graphing,
     kind="reg",
     truncate=False,
     color="m",
@@ -48,6 +60,37 @@ g = sns.jointplot(
     scatter_kws={"s": 20},
 )
 plt.show()
+
+
+# Lasso
+reg = Lasso()
+reg.fit(train_x, train_y)
+print("[magenta]Lasso[/magenta]")
+print("Intercept:\t", reg.intercept_)
+print("Coefficients:\t", reg.coef_[0])
+y_pred = reg.predict(test_x)
+table.add_row(
+    "Lasso",
+    f"{r2_score(test_y.astype(float), y_pred):.3f}",
+    f"{mean_squared_error(test_y.astype(float), y_pred):.3f}",
+    f"{mean_absolute_error(test_y.astype(float), y_pred):.3f}",
+)
+
+
+# Ridge
+reg = Ridge()
+reg.fit(train_x, train_y)
+print("[magenta]Ridge[/magenta]")
+print("Intercept:\t", reg.intercept_)
+print("Coefficients:\t", reg.coef_[0])
+y_pred = reg.predict(test_x)
+table.add_row(
+    "Ridge",
+    f"{r2_score(test_y.astype(float), y_pred):.3f}",
+    f"{mean_squared_error(test_y.astype(float), y_pred):.3f}",
+    f"{mean_absolute_error(test_y.astype(float), y_pred):.3f}",
+)
+
 
 # Multivariate
 train_x, train_y, vali_x, vali_y, test_x, test_y = utils.load_tracks_xyz(
@@ -78,10 +121,19 @@ label_encoders[("track", "favorites")] = le
 reg = LinearRegression()
 reg.fit(train_x, train_y)
 
+print("[magenta]Multivariate[/magenta]")
+print("Intercept: ", reg.intercept_)
 print("Coefficients: \n", reg.coef_)
-print("Intercept: \n", reg.intercept_)
 
 y_pred = reg.predict(test_x)
-print("R2: %.3f" % r2_score(test_y, y_pred))
-print("MSE: %.3f" % mean_squared_error(test_y, y_pred))
-print("MAE: %.3f" % mean_absolute_error(test_y, y_pred))
+
+table.add_row(
+    "Multivariate linear",
+    f"{r2_score(test_y, y_pred):.3f}",
+    f"{mean_squared_error(test_y, y_pred):.3f}",
+    f"{mean_absolute_error(test_y, y_pred):.3f}",
+)
+
+console.print(table)
+console.print("[red underline]MSE[/red underline]: Mean squared error")
+console.print("[red underline]MAE[/red underline]: Mean absolute error")
