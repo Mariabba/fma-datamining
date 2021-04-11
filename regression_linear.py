4120e26d9cb2be007e015a5244371199c86a0eb3
Fabio <4898010+gatto@users.noreply.github.com>
2021-04-11 11:32:01 +0200

    linear regr and graph assets

--- regression_linear.py
+++ regression_linear.py
@@ -5,7 +5,7 @@ from rich import pretty, print
 from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import seaborn as sns
-
+from sklearn.preprocessing import LabelEncoder
 import utils
 
 pretty.install()
@@ -26,21 +26,11 @@ print("Coefficients: \n", reg.coef_)
 print("Intercept: \n", reg.intercept_)
 
 y_pred = reg.predict(test_x)
-print("R2: %.3f" % r2_score(test_y, y_pred))
-print("MSE: %.3f" % mean_squared_error(test_y, y_pred))
-print("MAE: %.3f" % mean_absolute_error(test_y, y_pred))
+print("R2: %.3f" % r2_score(test_y.astype(float), y_pred))
+print("MSE: %.3f" % mean_squared_error(test_y.astype(float), y_pred))
+print("MAE: %.3f" % mean_absolute_error(test_y.astype(float), y_pred))
 
-# i don't know what the fuck this graph code does - copypaste from Guidotti
-"""
-x_values = sorted(test_x[("track", "listens")])
-y_values = y_pred[np.argsort(test_x[("track", "listens")])]
-plt.scatter(test_x, test_y, color="black")
-plt.plot(x_values, y_values, color="blue", linewidth=3)
-plt.title("Scatterplot with regression line")
-plt.show()
-"""
-
-# this is better graphing, using seaborn
+# Graphing
 train_x = train_x.join(train_y).astype(float)
 train_x.columns = train_x.columns.get_level_values(1)
 sns.set_theme(style="darkgrid")
@@ -60,21 +50,38 @@ g = sns.jointplot(
 plt.show()
 
 # Multivariate
-column2drop = []
+train_x, train_y, vali_x, vali_y, test_x, test_y = utils.load_tracks_xyz(
+    buckets="continuous", extractclass=("track", "favorites")
+).values()
 
-tracks = utils.load_small_tracks(buckets="continuous")
+label_encoders = dict()
+column2encode = [
+    ("track", "language_code"),
+    ("album", "type"),
+    ("track", "license"),
+    ("album", "date_created"),
+    ("artist", "date_created"),
+    ("track", "date_created"),
+    ("track", "duration"),
+]
+for col in column2encode:
+    le = LabelEncoder()
+    train_x[col] = le.fit_transform(train_x[col])
+    test_x[col] = le.fit_transform(test_x[col])
+    label_encoders[col] = le
+
+le = LabelEncoder()
+train_y = le.fit_transform(train_y)
+test_y = le.fit_transform(test_y)
+label_encoders[("track", "favorites")] = le
 
 reg = LinearRegression()
-reg.fit(all_dfs["train_x"].values, all_dfs["train_y"])
-
-print(reg)
+reg.fit(train_x, train_y)
 
 print("Coefficients: \n", reg.coef_)
 print("Intercept: \n", reg.intercept_)
 
-
-y_pred = reg.predict(all_dfs["test_x"])
-print("R2: %.3f" % r2_score(all_dfs["test_y"], y_pred))
-
-print("MSE: %.3f" % mean_squared_error(y_test, y_pred))
-print("MAE: %.3f" % mean_absolute_error(y_test, y_pred))
+y_pred = reg.predict(test_x)
+print("R2: %.3f" % r2_score(test_y, y_pred))
+print("MSE: %.3f" % mean_squared_error(test_y, y_pred))
+print("MAE: %.3f" % mean_absolute_error(test_y, y_pred))
