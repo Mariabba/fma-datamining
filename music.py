0c9b9e75b1d3dce8c4a3f694aa226a91214f1f5f
Fabio <4898010+gatto@users.noreply.github.com>
2021-04-24 12:09:02 +0200

    starting to load

--- music.py
+++ music.py
@@ -1,35 +1,52 @@
 import attr
+from numpy import mask_indices
 import pandas as pd
 import librosa
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 @attr.s
 class MusicDB(object):
-    _dataframe = attr.ib()
+    df = attr.ib()
 
-    @_dataframe.default
+    @df.default
     def _dataframe_default(self):
-        # check if we have the pickle
-        # if self._dataframe_picklecheck: etc
-        # return self._dataframe_pickeload
-
+        self.pick = self._dataframe_pickleload()
+        if self.pick:
+            return self.pick
         # if not, populate
         return self._dataframe_populate()
 
-    def _dataframe_picklecheck(self):
-        # check if we have pickle
-
-        pass
+    # start of private methods
+    def _dataframe_pickleload(self):
+        path_to_pickle = Path("data/picks/small.pkl")
+        try:
+            self.pipi = {}
+            self.pipi["df"] = pd.read_pickle(path_to_pickle)
+            self.pipi["status"] = True
+        except FileNotFoundError:
+            return False
+        return self.pipi
 
     def _dataframe_populate(self):
         # extract using librosa from tracks
         self.df = pd.DataFrame(columns=list(range(30)))
         self.df = self.df.append([["miao" for _ in range(30)]], ignore_index=True)
 
-        # some printing just to understand how this works
-        print(self.df.info(), self.df.columns)
-        print(self.df.head())
+        y, sr = librosa.load("data/000002.mp3", sr=None)
+        miao = librosa.resample(y, sr, 90)
+        print(y, sr)
+        print(len(y))
+        print(miao, len(miao))
+        plt.plot(miao)
+        plt.show()
+        # -> 🐱 remember to save df
         return self.df
 
 
-music = MusicDB()
+if __name__ == "__main__":
+    music = MusicDB()
+    # some printing just to understand how this works
+    print(music.df.info(), music.df.columns)
+    print(music.df.head())
