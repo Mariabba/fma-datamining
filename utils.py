0beec0e938fe82ae91dadf872acbb086157255e3
Marianna Abbattista <37538269+Mariabba@users.noreply.github.com>
2021-05-18 18:22:23 +0200

    boh

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-#from langdetect import detect
+# from langdetect import detect
 from sklearn.preprocessing import MultiLabelBinarizer
 
 try:
