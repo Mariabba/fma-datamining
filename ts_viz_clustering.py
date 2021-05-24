8dbf026f9f7be04380161b4ac097efa0f1f10369
Fabio <4898010+gatto@users.noreply.github.com>
2021-05-24 12:44:51 +0200

    Update ts_viz_clustering.py

--- ts_viz_clustering.py
+++ ts_viz_clustering.py
@@ -24,34 +24,70 @@ data = {
 miao = pd.DataFrame.from_dict(
     data, orient="index", columns=["Euclidean distances", "DTW distances"]
 )
-print(miao.info())
+"""print(miao.info())
 print(miao.head(20))
 sns.lineplot(x=miao.index, y="Euclidean distances", data=miao)
 plt.show()
 
 sns.lineplot(x=miao.index, y="DTW distances", data=miao)
 plt.show()
-
+"""
 
 # centroid characterization
-"""musi = MusicDB()
+musi = MusicDB()
 
 km = TimeSeriesKMeans(n_clusters=5, metric="dtw", max_iter=5, random_state=5138)
 km.fit(musi.sax)
 
 miao = pd.DataFrame(km.cluster_centers_.squeeze())
-miao.to_csv("miaomiao.csv")
-"""
-miao = pd.read_csv("miaomiao.csv", index_col=0)
-print(miao)
-print(miao.info())
+# miao.to_csv("miaomiao.csv")
 
-w = 5
-with5 = ((miao - miao.mean()) / miao.std()).rolling(window=w).mean()
-plt.plot(with5.T)
+# miao = pd.read_csv("miaomiao.csv", index_col=0)
+
+w = 20
+for i in range(5):
+    with5 = (
+        ((miao.iloc[i] - miao.iloc[i].mean()) / miao.iloc[i].std())
+        .rolling(window=w)
+        .mean()
+    )
+    plt.plot(with5)
 plt.show()
 
-w = 10
-with10 = ((miao - miao.mean()) / miao.std()).rolling(window=w).mean()
-plt.plot(with10.T)
+musi.feat["ClusterLabel"] = km.labels_
+musi.feat = musi.feat.drop(["enc_genre"], axis=1)
+
+musi.feat = musi.feat.groupby(["genre", "ClusterLabel"], as_index=False).size()
+musi.feat = musi.feat[musi.feat["size"] != 0]
+musi.feat = musi.feat.sort_values(by=["ClusterLabel"])
+musi.feat.index = musi.feat["genre"]
+
+cluster1 = musi.feat[musi.feat["ClusterLabel"] == 1].sort_values(by=["size"])
+cluster1["size"].plot(kind="bar", x="genre")
+plt.title("Tracks genre distribution - cluster 1")
+plt.xticks(rotation=18)
+plt.show()
+
+cluster4 = musi.feat[musi.feat["ClusterLabel"] == 2].sort_values(by=["size"])
+cluster4["size"].plot(kind="bar", x="genre")
+plt.title("Tracks genre distribution - cluster 2")
+plt.xticks(rotation=18)
+plt.show()
+
+cluster5 = musi.feat[musi.feat["ClusterLabel"] == 3].sort_values(by=["size"])
+cluster5["size"].plot(kind="bar", x="genre")
+plt.title("Tracks genre distribution - cluster 3")
+plt.xticks(rotation=18)
+plt.show()
+
+cluster6 = musi.feat[musi.feat["ClusterLabel"] == 4].sort_values(by=["size"])
+cluster6["size"].plot(kind="bar", x="genre")
+plt.title("Tracks genre distribution - cluster 4")
+plt.xticks(rotation=18)
+plt.show()
+
+cluster1 = musi.feat[musi.feat["ClusterLabel"] == 5].sort_values(by=["size"])
+cluster1["size"].plot(kind="bar", x="genre")
+plt.title("Tracks genre distribution - cluster 5")
+plt.xticks(rotation=18)
 plt.show()
