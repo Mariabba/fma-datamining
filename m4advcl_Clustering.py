d27331561e0954c194bb3e06e3a2dc3bd1c6288a
Marianna Abbattista <37538269+Mariabba@users.noreply.github.com>
2021-06-02 15:32:19 +0200

    esaurimento

--- m4advcl_Clustering.py
+++ m4advcl_Clustering.py
@@ -4,8 +4,14 @@ from nltk.sem.logic import printtype
 from pyclustering.cluster import xmeans, cluster_visualizer, cluster_visualizer_multidim
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from pyclustering.cluster.silhouette import silhouette
+from pyclustering.utils import read_sample
 from sklearn.preprocessing import LabelEncoder
 from array import array
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+from sklearn.cluster import OPTICS
 import utils
 
 df = utils.load_tracks(
@@ -44,40 +50,91 @@ for col in column2encode:
 
 
 print(df.info())
+numeric_columns = [
+    ("album", "comments"),
+    ("album", "date_created"),
+    ("album", "favorites"),
+    ("album", "listens"),
+    ("album", "type"),
+    ("artist", "comments"),
+    ("artist", "date_created"),
+    ("artist", "favorites"),
+    ("track", "comments"),
+    ("track", "date_created"),
+    ("track", "duration"),
+    ("track", "favorites"),
+    ("track", "interest"),
+    ("track", "listens"),
+    ("artist", "active_year_end"),
+    ("artist", "wikipedia_page"),
+    ("track", "composer"),
+    ("track", "information"),
+    ("track", "lyricist"),
+    ("track", "publisher"),
+    ("album", "engineer"),
+    ("album", "information"),
+    ("artist", "bio"),
+    ("album", "producer"),
+    ("artist", "website"),
+]
 
-"""MAKING ADVANCED CLUSTER"""
+X = df[numeric_columns].values
+print("dataset:", X.shape)
+
+
+"""MAKING ADVANCED CLUSTER X-MEANS"""
 amount_initial_centers = 2
-initial_centers = kmeans_plusplus_initializer(
-    df.values, amount_initial_centers
-).initialize()
+initial_centers = kmeans_plusplus_initializer(X, amount_initial_centers).initialize()
 
-xmeans_instance = xmeans.xmeans(df.values, initial_centers, 20)
+xmeans_instance = xmeans.xmeans(X)
 xmeans_instance.process()
 
 
 # Extract clustering results: clusters and their centers
 clusters = xmeans_instance.get_clusters()
 centers = xmeans_instance.get_centers()
-res = array("f", clusters)
-print(type(clusters))
 
-
-plt.plot(clusters)
-plt.show()
+a_cluster = np.asarray(clusters)
+a_centers = np.asarray(centers, dtype=object)
+print("dopo cluster", type(a_cluster))
+print("dopo centers", type(a_centers))
+# print(a_cluster)
 
 
 # Calculate Silhouette score
-# score = silhouette(df.values, clusters).process().get_score()
-# score.plot()
+# print("sil making")
+# score = silhouette(X, clusters).process().get_score()
+# print("Scores: '%s'" % str(score))
 
 
-"""Visual Guidotti"""
+"""# Visual Guidotti
 # print("score", score)
 i = df.columns.values.tolist().index(("album", "listens"))
 j = df.columns.values.tolist().index(("track", "favorites"))
 
+sns.set()
 for indexes in clusters:
-    plt.scatter(df.values[indexes, i], df.values[indexes, j], alpha=0.4)
+    plt.scatter(X, X, alpha=0.4)
 for c in centers:
-    plt.scatter(c[i], c[j], s=100, edgecolors="k")
+    plt.scatter(c, c, s=50, edgecolors="k")
+plt.show()
+"""
+"""EMBALANCE LEARNING"""
+# ORIGINAL PCA
+
+print(X.shape)
+pca = PCA(n_components=25)
+pca.fit(X)
+X_pca = pca.transform(X)
+print("pcs shape", X_pca.shape)
+
+plt.scatter(
+    X_pca[:, 0],
+    X_pca[:, 1],
+    cmap="Set2",
+    edgecolor="k",
+    alpha=0.5,
+)
+plt.scatter(a_cluster, a_cluster, alpha=0.3)
+plt.title("Standard KNN-PCA")
 plt.show()
