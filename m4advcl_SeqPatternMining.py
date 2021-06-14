812f500b6e98f6fa036cef77c06f62db4ec9eb41
Saverio <s.telera@studenti.uniba.it>
2021-06-14 12:53:45 +0200

    Codice funziona

--- m4advcl_SeqPatternMining.py
+++ m4advcl_SeqPatternMining.py
@@ -7,28 +7,29 @@ musi = MusicDB()
 
 
 print(musi.sax.info())
-db = musi.sax.head(5).values
+db = musi.sax.values
 
 map_symbols = {k: v for v, k in enumerate(np.unique(db.ravel()))}
 print(map_symbols)
 
 seq = np.array([map_symbols[v] for v in db.ravel()])
-print(seq)
+
 X_seq = list()
 for x in db:
     X_seq.append([map_symbols.get(v, -1) for v in x.ravel()])
 
-
+print(X_seq)
 ps = PrefixSpan(X_seq)
+ps.minlen = 100
+ps.maxlen = 100
 
 print("ps.frequent")
-# miao = ps.frequent(3)
+print(ps.frequent(4000))
 
-# print(miao)
-
+"""
 print("ps.topk")
-# print(ps.topk(10))
-
+print(ps.topk(10))
+"""
 """
 # minsup standard = 2
 p.setminsup(5)
